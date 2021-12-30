25c441d49d6a26436de13714431d60bcd77bbe4f
User <uriyderechey@gmail.com>
2021-12-30 18:01:32 +0200

    add lazy scroll

--- markup/static/js/custom.js
+++ markup/static/js/custom.js
@@ -17,4 +17,39 @@ $(document).ready(function () {
             $(".js-header").removeClass("is-scroll");
         }
     });
+
+    $('.js-to-item').on('click', function () {
+        scrollToItem($(this));
+        $('.js-header-burger').removeClass('open');
+        $('.header').removeClass('open');
+        $('.js-header-toggle nav').removeClass('open');
+        $('.header__burger-letter').removeClass('open');
+        $('.header__nav').removeClass('open');
+        $('body').removeClass('fixed-position');
+    });
 });
+
+
+// scroll to element
+function scrollToItem(elem) {
+    var el = $(elem).attr('href').slice(1),
+        elToScroll = $(`#${el}`),
+        navHeight = ($('.js-header').outerHeight()) + 20,
+        time = 600,
+        gap = 85,
+        offsetTop = elToScroll.offset().top,
+        totalScroll = offsetTop - navHeight - gap;
+    $('body,html').animate({
+        scrollTop: totalScroll
+    }, time);
+
+    return false;
+}
+
+moveElem();
+
+function moveElem() {
+    var blockfrom = $('.js-remove--from').html();
+    $('.js-remove--to').html(blockfrom);
+    return false;
+}
