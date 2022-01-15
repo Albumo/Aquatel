313ab24abd5d669b0883fd8e601649dd49356d42
djtim <uriyderechey@gmail.com>
2022-01-15 14:33:14 +0200

    v2

--- markup/static/js/custom.js
+++ markup/static/js/custom.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-    var $burger = $(".js-header-burger");
-    var $nav = $(".js-header-toggle nav");
-    $burger.on("click", function () {
-        $(this).toggleClass("open");
-        $(".header").toggleClass("open");
-        $nav.toggleClass("open");
-        $("body").toggleClass("fixed-position");
+    var $burger = $('.js-header-burger');
+    var $nav = $('.js-header-toggle nav');
+    $burger.on('click', function () {
+        $(this).toggleClass('open');
+        $('.header').toggleClass('open');
+        $nav.toggleClass('open');
+        $('body').toggleClass('fixed-position');
     });
 
     $(window).scroll(function () {
         var height = $(window).scrollTop();
 
         if (height > 50) {
-            $(".js-header").addClass("is-scroll");
+            $('.js-header').addClass('is-scroll');
         } else {
-            $(".js-header").removeClass("is-scroll");
+            $('.js-header').removeClass('is-scroll');
         }
     });
 
